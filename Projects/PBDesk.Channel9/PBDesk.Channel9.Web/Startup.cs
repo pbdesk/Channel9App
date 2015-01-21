d4e8a6776e5416475cdd79ba34903225e1a8d643
Pinal Bhatt <pinal.bhatt@weightwatchers.com>
2015-01-21 17:05:11 -0500

    Initial MVC project created

---
+++ Projects/PBDesk.Channel9/PBDesk.Channel9.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PBDesk.Channel9.Web.Startup))]
+namespace PBDesk.Channel9.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
