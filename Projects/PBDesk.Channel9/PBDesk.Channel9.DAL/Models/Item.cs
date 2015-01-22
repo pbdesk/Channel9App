487abfdca4c7ba227ce6c88e0b4caa32c50aacc4
Pinal Bhatt <pinal.bhatt@weightwatchers.com>
2015-01-22 09:46:51 -0500

    Model classes created

---
+++ Projects/PBDesk.Channel9/PBDesk.Channel9.DAL/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PBDesk.EFRepository;
+
+namespace PBDesk.Channel9.DAL.Models
+{
+    public class Item : Entity
+    {
+    }
+}
